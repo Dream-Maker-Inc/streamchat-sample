f898d1f018740ab36656a2f683272f6df130c10b
wkdgus1164 <wkdgus1164@naver.com>
2022-08-31 23:42:39 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { StreamChat } from 'stream-chat'
+import { Chat, Channel, Window, ChannelHeader, MessageList, Thread, LoadingIndicator, MessageInput, ChannelList } from 'stream-chat-react'
+import 'stream-chat-react/dist/css/index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiKey = process.env.REACT_APP_STREAM_API_KEY
+
+const user = {
+  id: 'andy',
+  name: 'andy',
+  image: 'https://getstream.imgix.net/images/random_svg/FS.png'
 }
 
-export default App;
+export default function App() {
+  const [client, setClient] = useState(null)
+
+  useEffect(() => {
+    async function init() {
+      const chatClient = StreamChat.getInstance(apiKey)
+      await chatClient.connectUser(user, chatClient.devToken(user.id))
+      const channel = chatClient.channel('messaging', 'react-talk', {
+        image: 'https://getstream.imgix.net/images/random_svg/FS.png',
+        name: 'React!',
+        members: [user.id]
+      })
+
+      await channel.watch()
+
+      setClient(chatClient)
+    }
+
+    init()
+
+    if (client) return () => client.disconnectUser()
+  }, [])
+
+  if (!client) return <LoadingIndicator />
+
+  const filter = { members: { $in: ['andy'] } }
+
+  return (
+    <Chat client={client} theme="messaging light">
+      <ChannelList filters={filter} />
+      <Channel>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  )
+}
